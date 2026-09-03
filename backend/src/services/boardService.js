1c6043aa2e2649efccc79b35bed40ee5d95e5c1a
Tehan <tehananuda1510@gmail.com>
2026-09-03 11:38:54 +0530

    feat(backend): make getAllBoards async

--- backend/src/services/boardService.js
+++ backend/src/services/boardService.js
@@ -1,28 +1,6 @@
-// src/services/boardService.js
 import * as boardRepository from "../repositories/boardRepository.js";
 import { NotFoundError } from "../utils/AppError.js";
 
-export function getAllBoards() {
+export async function getAllBoards() {
   return boardRepository.findAll();
-}
-
-export function getBoardById(id) {
-  const board = boardRepository.findById(id);
-  if (!board) throw new NotFoundError("Board");
-  return board;
-}
-
-export function createBoard(data) {
-  return boardRepository.create(data);
-}
-
-export function updateBoard(id, patch) {
-  const updated = boardRepository.update(id, patch);
-  if (!updated) throw new NotFoundError("Board");
-  return updated;
-}
-
-export function deleteBoard(id) {
-  const deleted = boardRepository.remove(id);
-  if (!deleted) throw new NotFoundError("Board");
 }
